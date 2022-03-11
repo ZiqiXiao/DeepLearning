424552c4114166912b158c23d3132fe0c1db4d20
ZiqiXiao_m1 <xzqshawn@126.com>
2022-03-11 13:48:29 +0800

    update

--- OpenCV/Check.py
+++ OpenCV/Check.py
@@ -1,22 +1,20 @@
-import cv2 # opencv读取的格式是BGR，与matplotlib不同
+import cv2  # opencv读取的格式是BGR，与matplotlib不同
 import matplotlib.pyplot as plt
 import numpy as np
 
-vc = cv2.VideoCapture('/Users/xiaoziqi/Desktop/Code/deep-learning/OpenCV/data/test.mp4')
+pic_path = '/Users/xiaoziqi/Desktop/Code/deep-learning/OpenCV/data/cat1.jpeg'
+img = cv2.imread(pic_path)
 
-if vc.isOpened():
-    is_open, frame = vc.read()
-else:
-    is_open = False
 
-while open:
-    ret, frame = vc.read()
-    if frame is None:
-        break
-    if ret:
-        v_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('result', v_gray)
-        if cv2.waitKey(100) & 0xFF == 27:
-            break
-vc.release()
-cv2.destroyAllWindows()
+def cv_show(name_image, image):
+    # 图像的显示，也可以创建多个窗口
+    cv2.imshow(name_image, image)
+    # 等待时间，毫秒级，0表示任意键终止
+    cv2.waitKey(500)
+    cv2.destroyAllWindows()
+
+
+cur_img = img.copy()
+cur_img[:, :, 0] = 0
+cur_img[:, :, 1] = 0
+cv_show('cur_img', cur_img)
